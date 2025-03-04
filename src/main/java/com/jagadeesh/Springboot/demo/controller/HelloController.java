afac9a4afd854e2fb729f0a0a55712fa063144d1
Jagadeeswar <jagadeeshgajula9@gmail.com>
2025-03-04 14:24:48 +0530

    added message for controller

--- src/main/java/com/jagadeesh/Springboot/demo/controller/HelloController.java
+++ src/main/java/com/jagadeesh/Springboot/demo/controller/HelloController.java
@@ -13,6 +13,6 @@ public class HelloController {
     }
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World";
+        return "Welcome to hello controller";
     }
 }
