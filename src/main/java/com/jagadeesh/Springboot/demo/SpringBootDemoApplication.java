65a98881ade50c2fdfe9426921f6b32c76bf0943
Jagadeeswar <jagadeeshgajula9@gmail.com>
2025-03-05 12:23:15 +0530

    just added one print

--- src/main/java/com/jagadeesh/Springboot/demo/SpringBootDemoApplication.java
+++ src/main/java/com/jagadeesh/Springboot/demo/SpringBootDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootDemoApplication {
 	public static void main(String[] args) {
+		System.out.println("Hello Jagadeesh");
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 }
